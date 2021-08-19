5f672162d17ff08a71abe8ee7965f3197e1992c0
BentiPab <69325366+BentiPab@users.noreply.github.com>
2021-08-19 13:21:56 +0200

    first commit

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,16 @@ import { ApolloServer } from 'apollo-server';
 import typeDefs from './schema';
 import resolvers from './resolvers';
 
+const port = process.env.PORT || 5000
+
 const server = new ApolloServer({
+    cors: {
+		origin: '*',	
+		credentials: true},
     typeDefs: typeDefs,
     resolvers: resolvers
 });
 
-server.listen().then(({ url }) => {
+server.listen({port: port}).then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
 });
